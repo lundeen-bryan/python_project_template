9199a8fb6233fbb551ea219e370587d1d7de67b4
lundeen-bryan <lundeen-bryan@gmail.com>
2023-07-06 23:48:37 -0700

    refactored run.sh and added write_package.sh updated project.js to account for new sh scripts updated CHANGELOG.md

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,9 @@
 
 *Changelog created using the [Simple Changelog](https://marketplace.visualstudio.com/items?itemName=tobiaswaelde.vscode-simple-changelog) extension for VS Code.*
 
+## [0.0.2] - 2023-07-06
+- added write_package.sh and refactored run.sh
+
 ## [0.0.1] - 2023-07-01
 ### Changed
 - more detail in filetree
--- templates/run.sh
+++ templates/run.sh
@@ -22,30 +22,69 @@ add_hidden_attribute() {
 install_dependencies() {
   source "$venv_dir/Scripts/activate" || handle_error "Failed to activate virtual environment"
 
-  "$venv_dir/Scripts/activate" -m pip install --upgrade pip || handle_error "Failed to upgrade pip"
+  ask_upgrade_pip
 
-  if [[ ! -f requirements.txt ]]; then
-    handle_error "requirements.txt file does not exist in the current directory"
-  fi
-
-  "$venv_dir/Scripts/pip" install --no-deps --no-upgrade -r requirements.txt || handle_error "Failed to install dependencies from requirements.txt"
+  "$python_exe" -m pip install --no-deps -r requirements.txt || handle_error "Failed to install dependencies from requirements.txt"
 }
 
 # Function to validate user input
-validate_input() {
+validate_path() {
+  local path="${1}"
   # Convert any backslashes in the path to forward slashes
-  python_exe="${python_exe//\\//}"
+  path="${path//\\//}"
 
-  if [[ ! -x "$python_exe" ]]; then
-    echo "Invalid Python executable path. Exiting..."
-    exit 1
-  fi
-
-  if [[ "$run_main" != "yes" && "$run_main" != "no" ]]; then
-    echo "Invalid choice for running main.py. Exiting..."
+  if [[ ! -x "$path" ]]; then
+    echo "Invalid path. Exiting..."
     exit 1
   fi
 }
+
+# Function to validate yes/no user input
+validate_input() {
+  local input="${1}"
+
+  if [[ "$input" != "yes" && "$input" != "no" ]]; then
+    echo "Invalid choice. You can only enter yes or no to this question. Exiting..."
+    exit 1
+  fi
+}
+
+# Function to ask user if they want to upgrade pip
+ask_upgrade_pip() {
+  pip_version=$("$python_exe" -m pip --version)
+  echo "Current pip version is $pip_version"
+
+  read -p "Do you want to upgrade pip to the latest version? (yes/no) " upgrade_pip
+  validate_input "$upgrade_pip"
+
+  if [[ "$upgrade_pip" == "yes" ]]; then
+    echo "Upgrading pip. Please wait..."
+    "$python_exe" -m pip install --upgrade pip || handle_error "Failed to upgrade pip"
+  fi
+}
+
+# Preamble
+echo "This script has a few prerequisites:"
+echo "1. You should have a Python executable installed on your system."
+echo "2. You should know the path to the Python executable."
+echo "3. A requirements.txt file should be present in the same directory as this script."
+echo "4. The requirements.txt file should specify exact versions of the packages to be installed."
+echo ""
+echo "If these prerequisites are met, the script will perform the following actions:"
+echo "1. Create a Python virtual environment."
+echo "2. Hide the virtual environment directory."
+echo "3. Optionally, upgrade pip to the latest version in the virtual environment."
+echo "4. Install the packages listed in requirements.txt within the virtual environment, without upgrading existing packages or installing dependencies."
+echo "5. Optionally, run a Python script named main.py located in a subdirectory named src."
+echo ""
+
+# Ask for confirmation before proceeding
+read -p "Do you want to continue? (yes/no) " proceed
+validate_input "$proceed"
+if [[ "$proceed" != "yes" ]]; then
+  echo "Aborted by user. Exiting..."
+  exit 0
+fi
 
 # Clear the terminal screen
 clear
@@ -55,6 +94,7 @@ trap 'echo "Installation interrupted. Exiting..."; exit 1' INT
 
 # Prompt the user for the path to the Python executable
 read -p "Enter the path to the Python executable: " python_exe
+validate_path "$python_exe"
 
 # Prompt the user for the virtual environment name
 read -p "Enter the virtual environment name (default: .venv): " venv_name
@@ -62,16 +102,6 @@ venv_name=${venv_name:-.venv}
 
 # Set the virtual environment directory
 venv_dir="$PWD/$venv_name"
-
-# Validate user input
-validate_input
-
-# Ask for confirmation before creating the virtual environment
-read -p "About to create a new virtual environment at $venv_dir with Python executable $python_exe. Do you want to continue? (yes/no) " confirm
-if [[ "$confirm" != "yes" ]]; then
-  echo "Aborted by user. Exiting..."
-  exit 0
-fi
 
 # Create the virtual environment
 create_virtualenv
@@ -87,12 +117,12 @@ install_dependencies
 
 # Ask user if they want to run main.py
 read -p "Do you want to run main.py? (yes/no) " run_main
+validate_input "$run_main"
 
 # Execute the main.py Python script if the user answered "yes"
 if [[ "$run_main" == "yes" ]]; then
-  "$python_exe" ../src/main.py
+  "$python_exe" ./src/main.py
 fi
 
 # Pause the script and wait for the user to press Enter
 read -p "Hit Enter to close..."
-
